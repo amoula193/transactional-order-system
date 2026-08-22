579125e69f7c483392bb12c5680af0cf5d13bf07
amoula193 <amoucha14@gmail.com>
2026-08-22 17:09:31 +0100

    feat: configure PostgreSQL connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'postgres',
+        host: ConfigService.get<string>('DB_HOST'),
+        port: ConfigService.get<number>('DB_PORT'),
+        username: ConfigService.get<string>('DB_USERNAME'),
+        password: ConfigService.get<string>('DB_PASSWORD'),
+        name: ConfigService.get<string>('DB_NAME')
+      })
+
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
